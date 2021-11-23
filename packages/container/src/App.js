be6cabb80f2295b25116cec27f9585dd0d6b250b
Saif Eddine <saif.al-kallaf@loyaltylogistix.com>
2021-11-23 14:43:44 +0200

    Fixing the .yml issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 // Exporting the app component to be displayed as a container
-// that wraps all micro projects
+// that wraps all micro projects inside
 export default () => {
   return <MarketingApp />;
 };
