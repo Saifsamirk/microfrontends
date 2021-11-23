1458ab6ddcaeca833eb609addfcf73e68e598a40
Saif Eddine <saif.al-kallaf@loyaltylogistix.com>
2021-11-23 17:44:42 +0200

    randomize production classNames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 // Exporting the app component to be displayed as a container
 // that wraps all micro projects inside it
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
