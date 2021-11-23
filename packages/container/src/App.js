f48ff4ae9fc03759b9a8bf0559122186430b2fb8
Saif Eddine <saif.al-kallaf@loyaltylogistix.com>
2021-11-23 14:32:45 +0200

    Adding comments

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 // Exporting the app component to be displayed as a container
+// that wraps all micro projects
 export default () => {
   return <MarketingApp />;
 };
