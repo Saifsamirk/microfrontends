e92e272777b1f5df2caaf35682aa7bcb9001c1d4
Saif Eddine <saif.al-kallaf@loyaltylogistix.com>
2021-11-23 14:26:28 +0200

    Fixing the app component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
-// Exporting the app component
+// Exporting the app component to be displayed as a container
 export default () => {
   return <MarketingApp />;
 };
