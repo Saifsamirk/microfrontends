c7d74f657611ecb8abe3e9f7745b312d8f52c842
Saif Eddine <saif.al-kallaf@loyaltylogistix.com>
2021-11-23 17:12:48 +0200

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 // Exporting the app component to be displayed as a container
 // that wraps all micro projects inside it
 export default () => {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
+  );
 };
